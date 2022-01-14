d232960989b68b63f6db1fc3ba365cf230e8a866
Avila-BS <avila.xworkz@gmail.com>
2022-01-15 02:15:19 +0530

    bulk insertion using list

--- tourist_project/src/com/xworkz/tour/dao/MedicineDAO.java
+++ tourist_project/src/com/xworkz/tour/dao/MedicineDAO.java
@@ -1,7 +1,13 @@
 package com.xworkz.tour.dao;
 
+import java.util.List;
+
 import com.xworkz.tour.entity.MedicineEntity;
 
 public interface MedicineDAO {
 void put(MedicineEntity entity);
+default void putAll(List<MedicineEntity> entities)
+{
+	
+}
 }
